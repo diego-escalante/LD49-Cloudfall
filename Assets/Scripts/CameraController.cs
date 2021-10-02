6260834e8ca95362db2244c08489b55f6e4ba873
Diego Escalante <diego.escalante01@gmail.com>
2021-10-01 20:13:58 -0600

    Add basic camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,6 +10,19 @@ public class CameraController : MonoBehaviour {
     }
 
     private void Update() {
+        RaiseCameraWithPlayer();
+        CheckIfPlayerFell();
+    }
+
+    private void RaiseCameraWithPlayer() {
+        Vector3 camPos = transform.position;
+        if (camPos.y < player.position.y) {
+            camPos.y = player.position.y;
+            transform.position = camPos;
+        }
+    }
+
+    private void CheckIfPlayerFell() {
         Vector3 topOfPlayer = player.position + new Vector3(0, player.localScale.y / 2f, 0);
         if (cam.WorldToScreenPoint(topOfPlayer).y < 0) {
             EventManager.TriggerEvent(EventManager.Event.PlayerFell);
